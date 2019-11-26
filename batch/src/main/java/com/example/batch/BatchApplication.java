f723f8a750338a81b7035b825c54693e2a9a35d9
tadashi izuno <tadashi.izuno@gmail.com>
2019-11-26 21:52:18 +0900

    update

--- batch/src/main/java/com/example/batch/BatchApplication.java
+++ batch/src/main/java/com/example/batch/BatchApplication.java
@@ -2,12 +2,37 @@ package com.example.batch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.ExitCodeGenerator;
+import picocli.CommandLine;
+import picocli.CommandLine.IFactory;
 @SpringBootApplication
-public class BatchApplication {
+public class BatchApplication implements CommandLineRunner, ExitCodeGenerator {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BatchApplication.class, args);
-	}
+  private final IFactory factory; // auto-configured to inject PicocliSpringFactory
+  private final MyCommand myCommand; // your @picocli.CommandLine.Command-annotated class
+  private int exitCode;
+
+  // constructor injection
+  BatchApplication(final IFactory factory, final MyCommand myCommand) {
+    this.factory = factory;
+    this.myCommand = myCommand;
+  }
+
+  @Override
+  public void run(final String... args) {
+    // let picocli parse command line args and run the business logic
+    exitCode = new CommandLine(myCommand, factory).execute(args);
+  }
+
+  @Override
+  public int getExitCode() {
+    return exitCode;
+  }
+
+  public static void main(final String[] args) {
+      // let Spring instantiate and inject dependencies
+      System.exit(SpringApplication.exit(SpringApplication.run(BatchApplication.class, args)));
+  }
 
 }
--- batch/src/test/java/com/example/batch/BatchApplicationTests.java
+++ batch/src/test/java/com/example/batch/BatchApplicationTests.java
@@ -1,13 +1,9 @@
 package com.example.batch;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BatchApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
 
 }
